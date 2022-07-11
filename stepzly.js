3be6d6e5d8cb69ee655d8068e46e758aaaa9a327
Fábio Luiz de Souza Pereira <fabio_lsp@msn.com>
2022-07-10 22:53:53 -0300

    Base implementation for Loop (set/unset)

--- stepzly.js
+++ stepzly.js
@@ -20,13 +20,29 @@ class Steps {
         if (this.logExtension)
             this.logExtension(line)
     }
-    resolveValue(value) {
+    resolveParam(param, index) {
+        let returnParam = param;
+        if (typeof param == 'string' || value instanceof String) {
+            let indexMatch = param.match('{{index}}', 'g')
+            if (indexMatch) {
+                returnParam = returnParam.replace('{{index}}', index)
+            }
+        }
+        return returnParam
+    };
+    resolveValue(value, index) {
         let returnValue = value;
         if (typeof value == 'string' || value instanceof String) {
+            let indexMatch = value.match('{{index}}', 'g')
+            if (indexMatch) {
+                returnValue = returnValue.replace('{{index}}', index)
+            }
             let placeholderCheck = null;
-            while (placeholderCheck = this.placeholderRegex.exec(value)) {
+            while (placeholderCheck = this.placeholderRegex.exec(returnValue)) {
+                this.placeholderRegex.lastIndex = 0;
                 let newValue = p.retrieve(this.state, placeholderCheck[1])
-                returnValue = typeof newValue !== 'object' ? returnValue.replace(placeholderCheck[0], newValue) : newValue
+                returnValue = returnValue == placeholderCheck[0] || typeof newValue == 'object' ? newValue :
+                     returnValue.replace(placeholderCheck[0], newValue)
             }
         }
         return returnValue
@@ -41,8 +57,8 @@ class Steps {
         const target = set.response ? this.response : this.state
         for (let i = 0; i < set.to.length; i++) {
             if (set.from[i] !== undefined)
-                value = this.resolveValue(set.from[i])
-            p.assign(target, set.to[i], value, set.append)
+                value = this.resolveValue(set.from[i], step.loopIndex)
+            p.assign(target, this.resolveParam(set.to[i], step.loopIndex), value, set.append)
         }
         resolve(true)
     }
@@ -52,6 +68,7 @@ class Steps {
             unset.from = [unset.from]
         const target = unset.response ? this.response : this.state
         for (let i = 0; i < unset.from.length; i++) {
+            unset.from[i] = this.resolveParam(unset.from[i], step.loopIndex)
             p.remove(target, unset.from[i])
         }
         resolve(true)
@@ -68,11 +85,11 @@ class Steps {
         let fetch = step.fetch
         let headers = {}
         let bodyData = {}
-        fetch.url = this.resolveValue(fetch.url)
+        fetch.url = this.resolveValue(fetch.url, step.loopIndex)
         if (fetch.headers !== undefined)
             fetch.headers.map((header, index) => {
                 let headerKey = Object.keys(header)[0]
-                headers[headerKey] = this.resolveValue(header[headerKey])
+                headers[headerKey] = this.resolveValue(header[headerKey], step.loopIndex)
             })
         if (fetch.body) {
             if (typeof fetch.body == 'object')
@@ -98,17 +115,20 @@ class Steps {
         this.log(`Performing step ${step.name ?? 'unnamed step'}`)
         let loopPromises = []
         step.loopIndex = 0
-        if (step.loopOver)
+        step.loopCount = 1
+        if (step.loopOver) {
             step.loopOver = this.resolveValue(step.loopOver)
-
-        loopPromises.push(new Promise(resolve => {
-            if (step.set)
-                this.performSet(step, resolve)
-            if (step.unset)
-                this.performUnset(step.unset, resolve)
-            if (step.fetch)
-                this.performFetch(step.fetch, resolve)
-        }))
+            step.loopCount = step.loopOver.length
+        }
+        for (step.loopIndex; step.loopIndex < step.loopCount; step.loopIndex++)
+            loopPromises.push(new Promise(resolve => {
+                if (step.set)
+                    this.performSet(step, resolve)
+                if (step.unset)
+                    this.performUnset(step, resolve)
+                if (step.fetch)
+                    this.performFetch(step, resolve)
+            }))
         return Promise.all(loopPromises).then(new Promise((resolve) => {
             resolve(true);
         }));
